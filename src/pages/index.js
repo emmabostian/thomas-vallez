8ee3beb87dbaaec70de0a864a2cf3e5d65b7f0f1
Emma Wedekind <emma.wedekind@logmein.com>
2019-07-06 20:23:55 +0200

    Adding variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 const IndexPage = () => (
-  <Layout>
+  <Layout className="dark">
     <SEO title="Thomas Vallez" />
 
     <h1>Hey</h1>
